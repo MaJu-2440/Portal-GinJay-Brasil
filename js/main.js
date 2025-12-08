751b3ded098388abbf42045b03aea1276473e0e4
Maria Julia <mariajuliapacheco2020@gmail.com>
2025-12-08 12:01:51 -0300

    aplica funcionalidade de filtro dos produtos

--- js/main.js
+++ js/main.js
@@ -1,22 +1,57 @@
 const endpoint = "http://localhost:3000/produtos";
 
+const filtroBtns = document.querySelectorAll(".filtro-btn");
+const containerProdutos = document.querySelector(".produtos");
+
 async function buscaProdutos() {
-    try {
-        const response = await fetch(endpoint);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        return data;
+  try {
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-    catch (error) {
-        console.error('Error fetching data:', error);
-    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
 
+filtroBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const filtro = btn.getAttribute("data-filtro");
 
-buscaProdutos().then(produtos => {
-    produtos.forEach(produto => {
-        console.log(produto);
-    });
-    });
+    containerProdutos.innerHTML = "";
+    filtraProdutos(filtro);
+  });
+});
+
+function filtraProdutos(filtro) {
+  buscaProdutos().then((produtos) => {
+    const produtosFiltrados =
+      filtro === "todos"
+        ? produtos
+        : produtos.filter((produto) => produto.categoria === filtro);
+
+    carregaCards(produtosFiltrados);
+  });
+}
+filtraProdutos("todos");
+
+
+function carregaCards(produtos) {
+  produtos.forEach((produto) => {
+    const card = document.createElement("div");
+    card.classList.add("card-produto");
+
+    const img = document.createElement("img");
+    img.src = produto.url;
+    img.alt = produto.nome;
+    img.loading = "lazy";
+
+    const titulo = document.createElement("h3");
+    titulo.textContent = produto.nome;
+
+    card.append(img, titulo);
+    containerProdutos.appendChild(card);
+  });
+}
